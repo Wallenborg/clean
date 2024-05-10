e12361f6712723851eba6f18277e4026a30f94e0
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-05-10 13:06:08 +0200

    add custom hook useGetColorForHour

--- components/main-circle/MainCircle.jsx
+++ components/main-circle/MainCircle.jsx
@@ -1,8 +1,20 @@
 import "./MainCircle.css";
+import useGetColorForHour from "@/hooks/useGetColorForHour";
 
 export default function MainCircle() {
+  // Use the hook to get the color
+  const color = useGetColorForHour(
+    new Date().getHours(),
+    new Date().getMinutes()
+  );
+
+  // Inline style to set the background color dynamically
+  const circleStyle = {
+    backgroundColor: color,
+  };
+
   return (
-    <div className="maincircle-circle">
+    <div className="maincircle-circle" style={circleStyle}>
       <div className="maincircle-eyes">
         <span className="maincircle-eye"></span>
         <span className="maincircle-eye"></span>
