17e6c38f04bc2bb90a7414b939f14da2f4201cde
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-05-10 10:02:40 +0200

    initial commit clean

---
+++ app/clean/page.jsx
@@ -0,0 +1,7 @@
+export default async function CleanPage() {
+  return (
+    <div>
+      <h1>test Clean</h1>
+    </div>
+  );
+}
---
+++ app/sign-up/page.jsx
@@ -0,0 +1,7 @@
+export default function SignupPage() {
+  return (
+    <div>
+      <h1>test sign up</h1>
+    </div>
+  );
+}
