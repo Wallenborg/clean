6f8b8ebef27e1036c611327b89e3e1f80587aef7
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-05-10 14:20:55 +0200

    Add hero component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import Header from "@/components/header/Header";
+import Hero from "@/components/hero/Hero";
 
 export default function Home() {
   return (
     <main>
-      <Header includeNav={true} />
-      <h1>test landingpage</h1>
       <Header includeNav={false} />
+      <Hero />
     </main>
   );
 }
---
+++ components/hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero-section">
+      <h2 className="hero-text">
+        A graphical Experience
+        <br />
+        To
+        <br />
+        Track Your Sobriety Time
+      </h2>
+      <div className="small-circle small-circle-place01"></div>
+      <div className="big-circle big-circle-place01"></div>
+      {/* <div className="small-circle small-circle-place02"></div>
+      <div className="big-circle big-circle-place02"></div> */}
+    </section>
+  );
+}
