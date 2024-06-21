0b10e5e1169c67530fc9bd0a9b6a08ac9277e201
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-06-21 22:41:22 +0200

    add style to dropdown

--- components/dropdownmenu/DropdownMenu.jsx
+++ components/dropdownmenu/DropdownMenu.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 import useGetTimeSpan from "@/hooks/useGetTimeSpan";
 import Link from "next/link";
-import "./DropdownMenu";
+import "./DropdownMenu.css";
 
 export default function DropdownMenu({ startDate }) {
   const daysPassed = useGetTimeSpan(startDate);
 
   return (
     <div className="dropdown-menu">
-      <p>Days Passed: {daysPassed}</p>
-      <p>Re-start</p>
-      <p>Add Shortcut</p>
-      <Link href="/">Logout</Link>
+      <p className="dropdown-text">Days Passed: {daysPassed}</p>
+      <p className="dropdown-text">Re-start</p>
+      <p className="dropdown-text">Add Shortcut</p>
+      <Link className="dropdown-link" href="/">
+        <p className="dropdown-text">Logout</p>
+      </Link>
     </div>
   );
 }
