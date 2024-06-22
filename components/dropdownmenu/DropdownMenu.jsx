917b5b7233359166764c6ac01bdeae53c1639f40
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-06-22 06:41:26 +0200

    add weeks and hours to dropdown

--- components/dropdownmenu/DropdownMenu.jsx
+++ components/dropdownmenu/DropdownMenu.jsx
@@ -5,10 +5,14 @@ import "./Dropdown.css";
 
 export default function DropdownMenu({ startDate }) {
   const daysPassed = useGetTimeSpan(startDate);
+  const weeksPassed = Math.floor(daysPassed / 7);
+  const hoursPassed = daysPassed * 24;
 
   return (
     <div className="dropdown-menu">
       <p className="dropdown-text">Days Clean: {daysPassed}</p>
+      <p className="dropdown-text">Weeks Clean: {weeksPassed}</p>
+      <p className="dropdown-text">Hours Clean: {hoursPassed}</p>
       <p className="dropdown-text">Re-start</p>
       <p className="dropdown-text">Add Shortcut</p>
       <Link className="dropdown-link" href="/">
