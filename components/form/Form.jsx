702b64681a0d019a389e1f105bf041c9183ef0fa
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-06-23 15:03:51 +0200

    took away speach bubble

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -6,9 +6,9 @@ export default function Form() {
   return (
     <>
       <section className="form-container">
-        <div className="bubble">
+        {/* <div className="bubble">
           <p className="bubble-text">Hello!</p>
-        </div>
+        </div> */}
         <div className="form-maincircle-circle">
           <div className="form-maincircle-eyes">
             <span className="form-maincircle-eye"></span>
