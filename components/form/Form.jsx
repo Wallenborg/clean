f839d72b8cb827478608eee956e6c80f4624c1d3
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-06-23 15:52:18 +0200

    add gsap to animate popup circle

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -1,26 +1,78 @@
-import "./Form.css";
+// import "./Form.css";
+// import Link from "next/link";
+// import Button from "../button/Button";
+
+// export default function Form() {
+//   return (
+//     <>
+//       <section className="form-container">
+//         {/* <div className="bubble">
+//           <p className="bubble-text">Hello!</p>
+//         </div> */}
+//         <div className="form-maincircle-circle">
+//           <div className="form-maincircle-eyes">
+//             <span className="form-maincircle-eye"></span>
+//             <span className="form-maincircle-eye"></span>
+//           </div>
+//         </div>
+//         <div className="form-shape">
+//           <Link href="/clean">
+//             <Button text="Login" className="button-center-bottom" />
+//           </Link>
+//         </div>
+//       </section>
+//     </>
+//   );
+// }
+
+"use client";
+import { useEffect, useRef } from "react";
 import Link from "next/link";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Button from "../button/Button";
+import "./Form.css";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Form() {
+  const circleRef = useRef(null);
+
+  useEffect(() => {
+    const circle = circleRef.current;
+
+    gsap.fromTo(
+      circle,
+      { x: -40, y: 70, opacity: 0 },
+      {
+        x: 0,
+        y: 0,
+        rotate: 25,
+        opacity: 1,
+        scrollTrigger: {
+          trigger: circle,
+          start: "top 70%",
+          end: "bottom center",
+          scrub: true,
+          markers: true,
+        },
+      }
+    );
+  }, []);
+
   return (
-    <>
-      <section className="form-container">
-        {/* <div className="bubble">
-          <p className="bubble-text">Hello!</p>
-        </div> */}
-        <div className="form-maincircle-circle">
-          <div className="form-maincircle-eyes">
-            <span className="form-maincircle-eye"></span>
-            <span className="form-maincircle-eye"></span>
-          </div>
+    <section className="form-container">
+      <div className="form-maincircle-circle" ref={circleRef}>
+        <div className="form-maincircle-eyes">
+          <span className="form-maincircle-eye"></span>
+          <span className="form-maincircle-eye"></span>
         </div>
-        <div className="form-shape">
-          <Link href="/clean">
-            <Button text="Login" className="button-center-bottom" />
-          </Link>
-        </div>
-      </section>
-    </>
+      </div>
+      <div className="form-shape">
+        <Link href="/clean">
+          <Button text="Login" className="button-center-bottom" />
+        </Link>
+      </div>
+    </section>
   );
 }
