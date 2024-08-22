1f4bc39943f0d5acfbe06d0849088ef0d5e2bfd2
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-08-22 21:20:07 +0200

    add user icon

--- components/profile-container/ProfileContainer.jsx
+++ components/profile-container/ProfileContainer.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Button from "../button/Button";
 import { IoMdCloseCircleOutline } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import { RiUser3Line } from "react-icons/ri";
 import "./ProfileContainer.css";
 import { useStartDate } from "@/context/StartDateContext";
 
@@ -10,6 +12,8 @@ export default function ProfileContainer({ onClose }) {
   return (
     <section className="profile-container">
       <IoMdCloseCircleOutline className="profile-close" onClick={onClose} />
+      {/* <CgProfile className="user-icon" /> */}
+      <RiUser3Line className="user-icon" />
       <p className="profile-text big">username</p>
       <p className="profile-text">Clean Since</p>
       <p className="profile-text big">{startDate}</p>
