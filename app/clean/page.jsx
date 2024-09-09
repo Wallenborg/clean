2f4b6e3d714676472ccbbae9ba7001a611d9cab6
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-09-09 16:56:37 +0200

    try 2 on protected

--- app/clean/page.jsx
+++ app/clean/page.jsx
@@ -4,19 +4,16 @@ import { StartDateProvider } from "@/context/StartDateContext";
 import { UserProvider } from "@/context/UserContext";
 import Header from "@/components/header/Header";
 import DayCircle from "@/components/day-circle/DayCircle";
-import ProtectedRoute from "@/components/protected-route/ProtectedRoute";
 
 export default function CleanPage() {
   return (
-    <ProtectedRoute>
-      <UserProvider>
-        <StartDateProvider>
-          <div className="no-scroll">
-            <Header includeNav={true} />
-            <DayCircle />
-          </div>
-        </StartDateProvider>
-      </UserProvider>
-    </ProtectedRoute>
+    <UserProvider>
+      <StartDateProvider>
+        <div className="no-scroll">
+          <Header includeNav={true} />
+          <DayCircle />
+        </div>
+      </StartDateProvider>
+    </UserProvider>
   );
 }
