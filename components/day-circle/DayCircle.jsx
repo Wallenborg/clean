df3a158a6fc27644c0be430f0623ac589ce7bd8c
Wallenborg <niklas.wallenborg@hyperisland.se>
2024-09-11 09:27:39 +0200

    Update Project with New Circle Generation Method

--- components/day-circle/DayCircle.jsx
+++ components/day-circle/DayCircle.jsx
@@ -1,3 +1,105 @@
+// "use client";
+
+// import React, { useEffect, useRef } from "react";
+// import * as d3 from "d3";
+// import { useUser } from "@/context/UserContext";
+// import useGetTimeSpan from "@/hooks/useGetTimeSpan";
+// import useGenerateCircles from "@/hooks/useGenerateCircles";
+// import MainCircle from "../main-circle/MainCircle";
+// import "./DayCircle.css";
+
+// const DayCircle = () => {
+//   const { user } = useUser(); // Get the user object from context
+//   const startDate = user?.startDate || null; // Ensure startDate is accessed correctly
+//   const daysPassed = useGetTimeSpan(startDate); // Calculate the number of days passed since the start date
+//   const { circles, year } = useGenerateCircles(daysPassed); // Generate circles based on the days passed
+//   const svgRef = useRef(); // Reference to the SVG element for D3.js manipulation
+
+//   useEffect(() => {
+//     if (!svgRef.current) return; // Early exit if SVG reference is not available
+
+//     const svg = d3.select(svgRef.current);
+//     svg.selectAll("*").remove();
+
+//     const width = window.innerWidth;
+//     const height = window.innerHeight;
+//     const mainCircleRadius = 38;
+
+//     const largeCirclesGroup = svg.append("g").attr("id", "large-circles");
+//     const smallCirclesGroup = svg.append("g").attr("id", "small-circles");
+
+//     const generateRandomPosition = (index) => {
+//       const angle = Math.random() * 2 * Math.PI;
+//       const distance =
+//         mainCircleRadius + 50 + Math.random() * Math.log(index + 1) * 40;
+//       let x = width / 2 + distance * Math.cos(angle);
+//       let y = height / 2 + distance * Math.sin(angle);
+
+//       x = Math.max(10, Math.min(x, width - 20));
+
+//       return { x, y };
+//     };
+
+//     const isOverlapping = (newCircle, existingCircles) => {
+//       for (const circle of existingCircles) {
+//         const dx = newCircle.x - circle.x;
+//         const dy = newCircle.y - circle.y;
+//         const distance = Math.sqrt(dx * dx + dy * dy);
+//         if (distance < 15) {
+//           return true;
+//         }
+//       }
+//       return false;
+//     };
+
+//     const existingCircles = [];
+
+//     const animateCirclesSequentially = (circlesData) => {
+//       circlesData.forEach((circleData, index) => {
+//         setTimeout(() => {
+//           let position;
+//           let attempts = 0;
+//           do {
+//             position = generateRandomPosition(index);
+//             attempts++;
+//           } while (isOverlapping(position, existingCircles) && attempts < 100);
+
+//           existingCircles.push(position);
+
+//           const circleGroup =
+//             circleData.size === "large" ? largeCirclesGroup : smallCirclesGroup;
+
+//           circleGroup
+//             .append("circle")
+//             .attr("cx", position.x)
+//             .attr("cy", position.y)
+//             .attr("r", circleData.size === "large" ? 40 : 10)
+//             .attr("fill", "#f9f9f9")
+//             .attr("stroke", "#18191a")
+//             .attr("stroke-width", 1)
+//             .attr("class", "custom-circle");
+//         }, index * 200);
+//       });
+//     };
+
+//     animateCirclesSequentially(circles);
+//   }, [circles, svgRef]); // Add svgRef to dependencies
+
+//   return (
+//     <div className="daycircle-container">
+//       <svg ref={svgRef} width="100%" height="100%"></svg>
+//       <MainCircle startDate={startDate} />
+//       {year >= 1 && (
+//         <div className="year-display">
+//           + {year} {year === 1 ? "Year" : "Years"}
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default DayCircle;
+
 "use client";
 
 import React, { useEffect, useRef } from "react";
@@ -12,7 +114,7 @@ const DayCircle = () => {
   const { user } = useUser(); // Get the user object from context
   const startDate = user?.startDate || null; // Ensure startDate is accessed correctly
   const daysPassed = useGetTimeSpan(startDate); // Calculate the number of days passed since the start date
-  const { circles, year } = useGenerateCircles(daysPassed); // Generate circles based on the days passed
+  const { circles } = useGenerateCircles(daysPassed); // Generate circles based on the days passed
   const svgRef = useRef(); // Reference to the SVG element for D3.js manipulation
 
   useEffect(() => {
@@ -24,9 +126,6 @@ const DayCircle = () => {
     const width = window.innerWidth;
     const height = window.innerHeight;
     const mainCircleRadius = 38;
-
-    const largeCirclesGroup = svg.append("g").attr("id", "large-circles");
-    const smallCirclesGroup = svg.append("g").attr("id", "small-circles");
 
     const generateRandomPosition = (index) => {
       const angle = Math.random() * 2 * Math.PI;
@@ -56,6 +155,7 @@ const DayCircle = () => {
 
     const animateCirclesSequentially = (circlesData) => {
       circlesData.forEach((circleData, index) => {
+        if (index >= daysPassed) return; // break out from code why days passed
         setTimeout(() => {
           let position;
           let attempts = 0;
@@ -66,15 +166,14 @@ const DayCircle = () => {
 
           existingCircles.push(position);
 
-          const circleGroup =
-            circleData.size === "large" ? largeCirclesGroup : smallCirclesGroup;
+          const circleGroup = svg;
 
           circleGroup
             .append("circle")
             .attr("cx", position.x)
             .attr("cy", position.y)
-            .attr("r", circleData.size === "large" ? 40 : 10)
-            .attr("fill", "#f9f9f9")
+            .attr("r", 10)
+            .attr("fill", circleData.color) // color year
             .attr("stroke", "#18191a")
             .attr("stroke-width", 1)
             .attr("class", "custom-circle");
@@ -89,11 +188,6 @@ const DayCircle = () => {
     <div className="daycircle-container">
       <svg ref={svgRef} width="100%" height="100%"></svg>
       <MainCircle startDate={startDate} />
-      {year >= 1 && (
-        <div className="year-display">
-          + {year} {year === 1 ? "Year" : "Years"}
-        </div>
-      )}
     </div>
   );
 };
--- hooks/useGenerateCircles.jsx
+++ hooks/useGenerateCircles.jsx
@@ -1,4 +1,45 @@
+// import { useMemo, useState, useEffect } from "react";
+
+// function useGenerateCircles(daysPassed) {
+//   const [year, setYear] = useState(0);
+
+//   useEffect(() => {
+//     const currentYear = Math.floor(daysPassed / 365);
+//     setYear(currentYear);
+//   }, [daysPassed]);
+
+//   const circles = useMemo(() => {
+//     const currentCycleDays = (daysPassed % 365) + 1;
+
+//     return Array.from({ length: currentCycleDays }, (_, index) => {
+//       const dayNumber = index + 1;
+//       return {
+//         id: dayNumber,
+
+//         size: dayNumber % 28 === 0 ? "large" : "small",
+//       };
+//     });
+//   }, [daysPassed]);
+
+//   return { circles, year };
+// }
+
+// export default useGenerateCircles;
+
 import { useMemo, useState, useEffect } from "react";
+
+const colors = [
+  "#f9f9f9", // Bright off-white (Year 1)
+  "#ffd700", // Golden yellow (Year 2)
+  "#e28743", // Warm amber (Year 3)
+  "#8cb369", // Muted green (Year 4)
+  "#569cbf", // Soft blue (Year 5)
+  "#d36c96", // Dusty rose (Year 6)
+  "#b392ac", // Light lavender (Year 7)
+  "#6883ba", // Cool grayish-blue (Year 8)
+  "#c2c1c2", // Neutral silver (Year 9)
+  "#a68d60", // Soft taupe (Year 10)
+];
 
 function useGenerateCircles(daysPassed) {
   const [year, setYear] = useState(0);
@@ -8,15 +49,18 @@ function useGenerateCircles(daysPassed) {
     setYear(currentYear);
   }, [daysPassed]);
 
-  const circles = useMemo(() => {
-    const currentCycleDays = (daysPassed % 365) + 1;
+  const getColorForYear = (day) => {
+    const yearIndex = Math.floor(day / 365); // Get the year index based on the number of days
+    return colors[yearIndex % colors.length]; // Return the color for the corresponding year
+  };
 
-    return Array.from({ length: currentCycleDays }, (_, index) => {
+  const circles = useMemo(() => {
+    return Array.from({ length: daysPassed }, (_, index) => {
       const dayNumber = index + 1;
       return {
         id: dayNumber,
-
-        size: dayNumber % 28 === 0 ? "large" : "small",
+        year: Math.floor(dayNumber / 365), // Calculate the year for each day
+        color: getColorForYear(dayNumber), // Assign color based on the year for each day
       };
     });
   }, [daysPassed]);
